d53f5b1fb7761ce8a47e58bfd4e9408f23641793
user <chldbfkr456@naver.com>
2023-07-07 16:39:57 +0900

    230707 Commit - 로그인 구현

---
+++ src/main/java/side_project/SessionConst.java
@@ -0,0 +1,5 @@
+package side_project;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
--- src/main/java/side_project/ranking_system/controller/LoginController.java
+++ src/main/java/side_project/ranking_system/controller/LoginController.java
@@ -1,9 +1,48 @@
 package side_project.ranking_system.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import side_project.SessionConst;
+import side_project.ranking_system.domain.Member;
+import side_project.ranking_system.service.LoginService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class LoginController {
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public String login(String loginId, String loginPassword,
+                        BindingResult bindingResult, HttpServletRequest request) {
+        Member loginMember = loginService.login(loginId, loginPassword);
+
+        if(loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+
+            return "아이디 또는 비밀번호가 맞지 않습니다.";
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "로그인이 성공하였습니다.";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if(session != null) {
+            session.invalidate();
+        }
+
+        return "로그아웃되었습니다.";
+    }
 }
